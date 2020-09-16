3c024c1957538a355e6f11dd6828a4c1babe844b
Sean Kelliher <sbkelliher@icloud.com>
2020-09-16 17:05:15 -0400

    Add initial code to add comments to homepage.

--- home-comments.php
+++ home-comments.php
@@ -1,21 +1,22 @@
-<div class="home-section-comments"> <!--home-section-news-->
-    <header class="comments-header"> <!--news-header-->
-        <h2 class="section-title"><?php echo of_get_option( "news-heading" ); ?></h2>
+<div class="home-section-news"> <!--home-section-news-->
+    
+    <header class="news-header"> <!--news-header-->
+        <h2 class="section-title">Recent Comments</h2>
     </header>
 
-    <ul id="recentcomments">
+            <?php $recent_comments = get_comments( array( 
+                    'number' => 5, // number of comments to retrieve.
+                    'status' => 'approve', // we only want approved comments.
+                    'post_status' => 'publish' // limit to published comments.
+                ) );
 
-        <?php $recent_comments = get_comments( array( 
-                'number' => 5, // number of comments to retrieve.
-                'status' => 'approve', // we only want approved comments.
-                'post_status' => 'publish' // limit to published comments.
-            ) );
-
-            if ( $recent_comments ) {
-                foreach ( (array) $recent_comments as $comment ) {
-                    echo '<li><a href="' . esc_url( get_comment_link( $comment ) ) . '">' . get_the_title( $comment->comment_post_ID ) . '</a></li>';
+                if ( $recent_comments ) {
+                    foreach ( (array) $recent_comments as $comment ) {
+                        echo '<section class="news-post">' . get_comment_author( $comment ) . ' on <a href="' . esc_url( get_comment_link( $comment ) ) . '">' . get_the_title( $comment->comment_post_ID ) . '</a></section>';
+                        }
                     }
-                }
-     </ul>
-     
+            ?>
+
+    </section>
+
 </div>
