ccc8c165bd3c84f1e3a683144b71d40b83ca9101
Sean Kelliher <sbkelliher@icloud.com>
2020-09-16 13:38:10 -0400

    Add 'page-home.php' file.

---
+++ page-home.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Template Name: Home Page Template
+ * Description: A page template with a map
+ *
+ * @package WordPress
+ *@subpackage maspops
+ */
+
+get_header(); ?>
+
+          
+  <div class="row">
+
+    <div class="home-right">
+    <?php get_template_part( "content/home", "participate" ); ?>
+    <?php get_template_part( "content/home", "news" ); ?>
+    </div>
+    <div class="home-left">
+    <?php get_template_part( "content/home", "featured" ); ?>
+    <?php get_template_part( "content/home", "submissions" ); ?>
+    </div>
+    
+
+  </div>
+
+      
+
+
+<?php get_footer(); ?>
