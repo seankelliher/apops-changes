8c075eca2d6d31b1ac8b92ce875ce923ed4e4fdc
Sean Kelliher <sbkelliher@icloud.com>
2020-09-16 11:50:45 -0400

    Add 'home-news.php' file.

---
+++ home-news.php
@@ -0,0 +1,27 @@
+<div class="home-section-news">
+	<header class="news-header">
+		<h2 class="section-title"><?php echo of_get_option( "news-heading" ); ?></h2>
+	</header>
+
+	<?php query_posts('posts_per_page=10'); ?>
+	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+
+	<section class="news-post">
+		<header>
+			<time><?php echo get_the_date('n.j.y'); ?></time>
+			<h5><?php the_title(); ?> &nbsp;<a href="<?php the_permalink(); ?>">learn more</a></h5>
+		</header>
+	</section>
+
+	<?php endwhile; ?>
+	<?php endif; ?>
+	<?php wp_reset_query(); ?>
+
+	<div class="news-footer">
+		<?php 
+			$posts_page_id = get_option('page_for_posts');
+	    	$posts_page_url = get_page_uri($posts_page_id);
+		?>
+		<a href="<?php echo home_url( $posts_page_url ); ?>">All News &amp; Announcements &gt;</a>
+	</div>
+</div>
