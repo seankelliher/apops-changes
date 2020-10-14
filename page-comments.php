65ac2b9cbb7e2411997187962af02e3f74eaf0f4
Sean Kelliher <sbkelliher@icloud.com>
2020-10-14 16:11:01 -0400

    Add initial 'recent_comments' function.

--- page-comments.php
+++ page-comments.php
@@ -19,8 +19,24 @@ the_post();
             </header><!-- .entry-header -->
 
                 <?php the_content(); ?>
-                <?php wp_link_pages( array( 'before' => '<div class="page-link">' . __( 'Pages:', 'maspops' ), 'after' => '</div>' ) ); ?>
-                <?php pops_edit_bar(); ?>
+
+            <?php $recent_comments = get_comments( array( 
+                'number' => 10, // number of comments to retrieve.
+                'status' => 'approve', // we only want approved comments.
+                'post_status' => 'publish' // limit to published comments.
+            ) );
+
+            if ( $recent_comments ) {
+                foreach ( (array) $recent_comments as $comment ) {
+                    echo '<section class="news-post"><header><time>' . get_comment_date('n.j.y') . '</time><h5>' . $comment->comment_author . ' on <a href="' . esc_url( get_comment_link( $comment ) ) . '">' . get_the_title( $comment->comment_post_ID ). '</a>:</h5>' . $comment->comment_content . '</section>';
+                    }
+                }
+            ?>
+
+
+
+                <!--<?php wp_link_pages( array( 'before' => '<div class="page-link">' . __( 'Pages:', 'maspops' ), 'after' => '</div>' ) ); ?>
+                <?php pops_edit_bar(); ?>-->
 
         </article><!-- #content -->
         <?php get_sidebar(); ?>
