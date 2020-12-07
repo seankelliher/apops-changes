03204458a55d7689c0ccbdcfcdeada7d2775adad
Sean Kelliher <sbkelliher@icloud.com>
2020-12-07 13:24:38 -0500

    Remove extra '</section> tag. Fix indenting errors.

--- home-comments.php
+++ home-comments.php
@@ -4,20 +4,18 @@
         <h2 class="section-title">Recent Comments</h2>
     </header>
 
-            <?php $recent_comments = get_comments( array( 
-                    'number' => 5, // number of comments to retrieve.
-                    'status' => 'approve', // we only want approved comments.
-                    'post_status' => 'publish' // limit to published comments.
-                ) );
+    <?php $recent_comments = get_comments( array( 
+            'number' => 5, // number of comments to retrieve.
+            'status' => 'approve', // we only want approved comments.
+            'post_status' => 'publish' // limit to published comments.
+        ));
 
-                if ( $recent_comments ) {
-                    foreach ( (array) $recent_comments as $comment ) {
-                        echo '<section class="news-post"><header><time>' . get_comment_date('n.j.y') . '</time><h5>' . $comment->comment_author . ' on <a href=' . esc_url( get_comment_link( $comment ) ) . '>' . get_the_title( $comment->comment_post_ID ). '</a>:</h5>' . $comment->comment_content . '</section>';
-                        }
-                    }
-            ?>
-
-    </section>
+        if ( $recent_comments ) {
+            foreach ( (array) $recent_comments as $comment ) {
+                echo '<section class="news-post"><header><time>' . get_comment_date('n.j.y') . '</time><h5>' . $comment->comment_author . ' on <a href=' . esc_url( get_comment_link( $comment ) ) . '>' . get_the_title( $comment->comment_post_ID ). '</a>:</h5>' . $comment->comment_content . '</section>';
+            }
+        }
+    ?>
 
     <div class="news-footer">
         <a href="https://apops.mas.org/comments-archive/">All Comments &gt;</a>
